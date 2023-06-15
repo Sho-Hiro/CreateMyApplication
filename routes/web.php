afa4e7b28e8b92ef5f1d8889d191fb58ad1f9b2c
Sho-Hiro <avadablossom1998@gmail.com>
2023-06-15 16:09:25 +0900

    create_homeAndlogin

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('login/home');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
