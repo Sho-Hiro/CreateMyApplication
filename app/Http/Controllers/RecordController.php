c2924233dcfea9dfb267cf2603775fe42e45c2f8
Sho-Hiro <avadablossom1998@gmail.com>
2023-07-08 00:33:49 +0900

    try add record table

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Record;
+use App\Models\User;
 use App\Models\PaymentCategory;
 use App\Http\Requests\RecordMoneyRequest;
 
 class RecordController extends Controller
 {
-    public function record_money(Record $record,PaymentCategory $paymentCategory)
+    public function record_money(Record $record,PaymentCategory $paymentCategory )
     {
         $record->user_id = \Auth::id();
         return view('myApplication/record_money')->with(['records' => $record->get(), 'paymentCategories' => $paymentCategory->get()]);
@@ -19,8 +20,7 @@ class RecordController extends Controller
         $record->user_id = \Auth::id();
         $input = $request['record'];
         $post->fill($input)->save();
-        //return redirect('/myApplication/record_money/{user}');
+        // return redirect('/myApplication/record_money/{user}');
         return redirect('/myApplication/record_money');
     }
-  
 }
